245966bf6f2762aa1f89a594865b94718201fecd
LipeProgramador <felipeprogramador3g@hotmail.com>
2018-10-19 11:04:57 -0300

    Implentacao metodo disparo

--- PrototipoGame1/Assets/Teste.cs
+++ PrototipoGame1/Assets/Teste.cs
@@ -1,3 +1,18 @@
-public class Teste: Monobehavior
+public class Teste: Behavior
 {
+  private Rigidbody2D rigBall;
+  private void Start()
+  {
+    rigBall = GetComponent<Rigidbody2D>();
+  }
+  public void Dispara()
+  {
+    float x;
+    float y;
+    Vector2 dirForce;
+    x = force * Mathf.Cos(Mathf.Deg2Rad(transform.eulerAngle));
+    y = force * Mathf.Cos(Mathf.Deg2Rad(transform.eulerAngle));
+    dirForce = new Vector2(x,y);
+    rigBall.addForce(dirForce);
+  }
 }
