4b044870c5b23a65b29f2e6df5392c0749aac76c
LipeProgramador <felipeprogramador3g@hotmail.com>
2018-10-19 11:59:30 -0300

    Implementacao pontuacao2

--- PrototipoGame1/Assets/Pontuacao.cs
+++ PrototipoGame1/Assets/Pontuacao.cs
@@ -1,11 +1,19 @@
 public class Pontuacao: Behavior
 {
-  int pontos = 0;
+  private int pontos = 0;
+  private Text text;
+  
+  private void Start()
+  {
+    text = GameObject.Find("pontos");
+  }
+  
   private void OntriggerEnter2D(Collision outro)
   {
     if(outro.gameObject.Comparetag("player"))
     {
       pontos++;
+      text.Text = pontos.String();
     }
   }
 }
