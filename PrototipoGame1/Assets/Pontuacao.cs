da7f2954cc48cf7be85d327ff8f0118c541a977d
LipeProgramador <felipeprogramador3g@hotmail.com>
2018-10-19 21:01:08 -0300

    Correcao do codigo

--- PrototipoGame1/Assets/Pontuacao.cs
+++ PrototipoGame1/Assets/Pontuacao.cs
@@ -1,19 +1,23 @@
-public class Pontuacao: Behavior
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pontuacao: MonoBehaviour
 {
   private int pontos = 0;
   private Text text;
   
   private void Start()
   {
-    text = GameObject.Find("pontos");
+        text = GameObject.Find("Pontos").gameObject.GetComponent<Text>() ;
   }
   
   private void OntriggerEnter2D(Collision outro)
   {
-    if(outro.gameObject.Comparetag("player"))
+    if(outro.gameObject.CompareTag("player"))
     {
       pontos++;
-      text.Text = pontos.String();
+      text.text = pontos.ToString();
     }
   }
 }
--- PrototipoGame1/Assets/Teste.cs
+++ PrototipoGame1/Assets/Teste.cs
@@ -1,18 +1,24 @@
-public class Teste: Behavior
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teste: MonoBehaviour
 {
-  private Rigidbody2D rigBall;
-  private void Start()
-  {
-    rigBall = GetComponent<Rigidbody2D>();
-  }
-  public void Dispara()
-  {
-    float x;
-    float y;
-    Vector2 dirForce;
-    x = force * Mathf.Cos(Mathf.Deg2Rad(transform.eulerAngle));
-    y = force * Mathf.Cos(Mathf.Deg2Rad(transform.eulerAngle));
-    dirForce = new Vector2(x,y);
-    rigBall.addForce(dirForce);
-  }
+    private Rigidbody2D rigBall;
+    float force = 20f;
+
+    private void Start()
+    {
+        rigBall = GetComponent<Rigidbody2D>();
+    }
+    public void Dispara()
+    {
+        float x;
+        float y;
+        Vector2 dirForce;
+        x = force * Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z);
+        y = force * Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z);
+        dirForce = new Vector2(x,y);
+        rigBall.AddForce(dirForce);
+    }
 }
